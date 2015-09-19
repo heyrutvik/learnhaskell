0a13d1b1b143ac9e1c2cfa39592e0fdfdffad578
RR <heyrutvik@gmail.com>
2015-09-19 23:43:49 +0530

    ch01 #4

--- realWorld/ch01/exercises.hs
+++ realWorld/ch01/exercises.hs
@@ -1,5 +1,8 @@
--- count the number of lines of input
 
 -- #3 count the number of words in a input file
 countWords = interact wordCount
   where wordCount input = show (length (words input)) ++ "\n"
+
+-- #4 number of characters
+countChars = interact wordCount
+  where wordCount input = show (length input) ++ "\n"
