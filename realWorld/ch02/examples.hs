72590a0e07ff6d3253d4ce18724893cb4da9f42e
RR <heyrutvik@gmail.com>
2015-09-20 10:42:57 +0530

    readWorld myDrop

--- realWorld/ch02/examples.hs
+++ realWorld/ch02/examples.hs
@@ -4,3 +4,9 @@ a is called type variable.
 -}
 doNothing :: a -> a
 doNothing x = x
+
+-- drop using if condition
+myDrop :: Int -> [a] -> [a]
+myDrop n xs = if n <= 0 || null xs
+              then xs
+              else myDrop (n-1) (tail xs)
