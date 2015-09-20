998dcd2e173d7a8e1c4b976f20d1052acee28052
RR <heyrutvik@gmail.com>
2015-09-20 12:07:17 +0530

    realWorld exercise

--- realWorld/ch02/exercises.hs
+++ realWorld/ch02/exercises.hs
@@ -8,3 +8,18 @@ False :: Bool
 [(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
 -- empty list can fit in any type of list or list of any type of list, I guess
 -}
+
+{-
+#2
+
+last :: [a] -> a
+From reading Type only,
+valid behaviour will return last element of type a from list of type a.
+return element from empty list function clearly cannot do.
+-}
+
+-- lastButOne returns element before last
+-- works only for list with two or more elements
+-- TODO improve function
+lastButOne :: [a] -> a
+lastButOne xs = xs !! (length xs - 2)
