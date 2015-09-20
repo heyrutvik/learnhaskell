ad3ff3580d7848734909304276bd6d70207f7d1c
RR <heyrutvik@gmail.com>
2015-09-20 09:44:34 +0530

    readWorld ch02

---
+++ realWorld/ch02/examples.hs
@@ -0,0 +1,6 @@
+{-
+polymorphic function.
+a is called type variable.
+-}
+doNothing :: a -> a
+doNothing x = x
---
+++ realWorld/ch02/exercises.hs
@@ -0,0 +1,10 @@
+{-
+#1
+
+False :: Bool
+
+(["foo", "bar"], 'a') :: ([[Char]], Char)
+
+[(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
+-- empty list can fit in any type of list or list of any type of list, I guess
+-}
