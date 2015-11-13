3792841a13905d984eed08a2289df130ae5723d2
RR <heyrutvik@gmail.com>
2015-11-13 14:19:42 +0530

    isPalindrome impl  - naive, will improve it.

--- realWorld/ch03/exercises.hs
+++ realWorld/ch03/exercises.hs
@@ -16,7 +16,7 @@ maybeChild2 = Node "parent" (Just (Node "left child" Nothing Nothing)) (Just (No
 
 -- get length of list
 -- getLength :: [a] -> Int is type what I thought it has, ghci told me this
-getLength :: Num a => [a] -> a
+getLength :: Num a => [t] -> a
 getLength [] = 0
 getLength (_:xs) = 1 + getLength xs
 
@@ -46,3 +46,23 @@ joinList (x:xs) ys = x : joinList xs ys
 -- turn list into a palindrome
 getPalindrome :: [a] -> [a]
 getPalindrome xs = joinList xs (getReverse xs)
+
+-- split list when two successive element is same
+splitWhenSame :: Eq a => [a] -> ([a], [a])
+splitWhenSame zs = go [] zs
+  where
+    go :: Eq a => [a] -> [a] -> ([a], [a])
+    go _ [] = ([], [])
+    go [] (y:ys) = go (y:[]) ys
+    go (x:xs) (y:ys)
+      | x == y = ((x:xs), (y:ys))
+      | otherwise = go (y:x:xs) (ys)
+
+-- check list is palindrome
+isPalindrome :: Eq t => [t] -> Bool
+isPalindrome [] = False
+isPalindrome xs =
+    let tuple = splitWhenSame xs
+        fs = fst (tuple)
+        ss = snd (tuple)
+    in (getLength fs == getLength ss) && (fs == ss)
