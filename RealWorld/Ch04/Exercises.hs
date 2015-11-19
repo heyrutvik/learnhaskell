51130f2799fa4965871cae3d632cb6b0b4b1f809
RR <heyrutvik@gmail.com>
2015-11-19 23:03:16 +0530

    splitWith  - splits its input list on every element for which the predicate returns False

--- RealWorld/Ch04/Exercises.hs
+++ RealWorld/Ch04/Exercises.hs
@@ -21,3 +21,21 @@ safeInit xs = Just (go xs)
                 go [] = []
                 go (_:[]) = []
                 go (y:ys) = y : (go ys)
+
+-- break split list on first true
+-- break' split line on first false
+break' :: (a -> Bool) -> [a] -> ([a], [a])
+break' p xs = ((takeWhile p xs), (dropWhile p xs))
+
+-- dropWhile drop elements while p is true
+-- dropWhile' drop elements while p is false
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p xs = dropWhile q xs
+                  where q n = not (p n)
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith _ [] = []
+splitWith p xs = let (pre, suf) = break' p xs
+                 in pre : case suf
+                          of
+                            zs -> splitWith p (dropWhile' p zs)
